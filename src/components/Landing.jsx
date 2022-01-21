4ba2b0c0a61bf0a5996fd45fc0e65dac63d03f2b
swati <91478967+swati-kaithwas@users.noreply.github.com>
2022-01-21 15:00:31 +0530

    navbar footer landing

---
+++ src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+export const Landing=()=>{
+    return (
+        <div className="bg-blue-700 h-screen bg-[url('https://www-growth.scdn.co/static/home/bursts.svg')] bg-[length:210%] sm:bg-[center_top_-6rem] bg-[center_top_2rem] sm:bg-[length:160%]">
+            <div className="text-[70px] text-green-500 font-bold leading-none text-left ml-8 pt-16 sm:ml-48 sm:text-[150px]">Listening is</div>
+            <div className="text-[70px] text-green-500 font-bold leading-none text-right mr-8 sm:mr-48 sm:text-[150px]">everything</div>
+            <div className="text-green-500 mt-8 px-20 text-center text-xl">Millions of songs and podcasts. No credit card needed.</div>
+            <div className="text-center">
+            <button className="bg-green-500 py-4 px-8 rounded-full m-8 text-blue-700 font-bold hover:scale-105">GET SPOTIFY FREE</button>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import react, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import {GiHamburgerMenu} from "react-icons/gi"
+// import ScrollIntoView from "react-scroll-into-view";
+export const Navbar=()=>{
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <nav className="sticky top-0 z-20 items-center justify-between p-4 text-xl bg-black md:flex text-white h-16">
+      <div className="flex items-center justify-between">
+        <h3 className="font-bold text-2xl ml-0 sm:ml-20"><img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" className="w-20 sm:w-32"/></h3>
+
+        {showNav ? (
+          <AiOutlineClose
+            onClick={() => setShowNav(!showNav)}
+            className="block w-10 h-10 p-2 cursor-pointer md:hidden "
+          />
+        ) : (
+          <GiHamburgerMenu
+            onClick={() => setShowNav(!showNav)}
+            className="block w-10 h-10 p-2 cursor-pointer md:hidden"
+          />
+        )}
+      </div>
+
+      <ul
+        className={
+          (showNav ? "left-8 text-3xl text-left" : "-left-full text-lg") +
+          " md:static fixed bottom-0 top-16 md:flex md:space-x-7 md:bg-transparent bg-black md:w-auto w-full md:text-white text-white md:space-y-0 space-y-5 p-2 transition-left ease-in pr-24"
+        }
+      >
+        <div>
+        <div className="cursor-pointer hover:text-green-500 font-bold">Premium</div>
+        </div>
+        <div>
+        <div className="cursor-pointer hover:text-green-500 font-bold">Support</div>
+        </div>
+        <div>
+        <div className="cursor-pointer hover:text-green-500 font-bold">Download</div>
+        </div>
+        <div>
+        <div className="">{showNav ? "—" : "|"}</div>
+        </div>
+        <div>
+        <div className={(showNav ? "font-light":"font-bold")+" cursor-pointer hover:text-green-500"}>Sign in</div>
+        </div>
+        <div>
+        <div className={(showNav ? "font-light":"font-bold")+" cursor-pointer hover:text-green-500"}>Log in</div>
+        </div>
+      </ul>
+    </nav>
+  );
+}
